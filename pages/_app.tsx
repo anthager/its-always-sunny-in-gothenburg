94ad8d9dad6376b4567fda4f9857758979a8ffd3
anthager <anthage@student.chalmers.se>
2021-03-06 16:08:52 +0100

    set up eslint, typescript and tailwind

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+// pages/_app.js
+import '../styles/globals.css'
+
+import type { AppProps } from 'next/app'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+const Index = () => {
+  return <div className="flex w-full h-screen bg-blue-500" />
+}
+
+export default Index
