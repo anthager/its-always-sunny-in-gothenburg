d82165442c2db689309663715977a6b50250d235
anthager <anthage@student.chalmers.se>
2021-03-08 20:52:46 +0100

    update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,17 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -12,9 +21,13 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'no-unused-vars': 'off',
+    'import/extensions': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 }
