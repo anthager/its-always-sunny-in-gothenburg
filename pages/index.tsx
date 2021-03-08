4b0f117ab8d7797333fc64dd264daad29ad49724
anthager <anthage@student.chalmers.se>
2021-03-08 23:07:49 +0100

    use css grid instead of table to make it easier make each cell span the whole width

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,29 +23,21 @@ const TABLE_CELL = 'px-3 capitalize'
 
 const Index = ({ max, min, average, median }: IndexProps): JSX.Element => {
   return (
-    <div className="flex flex-col items-center w-full h-screen text-gray-200 bg-blue-500">
-      <div className="flex pt-20 text-8xl space-x-8">
+    <div className="flex flex-col items-center w-full h-screen text-gray-300 bg-blue-900">
+      <div className="flex pt-20 text-5xl space-x-8 sm:text-8xl">
         <p className="font-medium">Göteborg</p>
       </div>
-      <p className="pt-4 text-xl">Temperatures the last 4 days:</p>
-      <table className="text-center text-xl">
-        <thead>
-          <tr>
-            <th className={TABLE_CELL}>min</th>
-            <th className={TABLE_CELL}>max</th>
-            <th className={TABLE_CELL}>average</th>
-            <th className={TABLE_CELL}>median</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{min}°c</td>
-            <td>{max}°c</td>
-            <td>{average}°c</td>
-            <td>{median}°c</td>
-          </tr>
-        </tbody>
-      </table>
+      <p className="text-md pt-4 font-light sm:text-lg">Temperatures the last 4 days:</p>
+      <div className="grid grid-cols-4 grid-rows-2 justify-items-center text-lg sm:text-xl">
+        <p className={TITLE_CELL}>average</p>
+        <p className={TITLE_CELL}>min</p>
+        <p className={TITLE_CELL}>max</p>
+        <p className={TITLE_CELL}>median</p>
+        <p>{average}°c</p>
+        <p>{min}°c</p>
+        <p>{max}°c</p>
+        <p>{median}°c</p>
+      </div>
     </div>
   )
 }
