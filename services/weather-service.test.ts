8757ae292bf4bec165b1a7b390c5770cf5ed9c2c
anthager <anthage@student.chalmers.se>
2021-03-08 21:23:11 +0100

    add WeatherService fetch data with the WeatherClient and calculate the requested statistics
    a few test cases are also added to along with setup for testing with jest and the necessary setup for jest

---
+++ services/weather-service.test.ts
@@ -0,0 +1,77 @@
+import OpenWeatherMapClient from '../clients/open-weather-map-client'
+import WeatherService from './weather-service'
+
+describe('WeatherApiService', () => {
+  const setupWeatherApiService = (jestFn: jest.Mock<any, any>): WeatherService => {
+    const weatherApiClientMock = ({
+      weatherForDay: jestFn,
+    } as unknown) as OpenWeatherMapClient
+    return new WeatherService(weatherApiClientMock)
+  }
+
+  it('should only use the temps of the last 24 hours when the last day contains all 24 hours', async () => {
+    const expected = {
+      min: 1,
+      average: 1,
+      max: 1,
+    }
+    const dataDay1 = {
+      hourly: new Array(24).fill({ temp: 0 }),
+    }
+    const dataDay2 = {
+      hourly: new Array(24).fill({ temp: 1 }),
+    }
+
+    const mockedFunction = jest
+      .fn()
+      .mockReturnValueOnce(Promise.resolve(dataDay1))
+      .mockReturnValueOnce(Promise.resolve(dataDay2))
+    const weatherApiService = setupWeatherApiService(mockedFunction)
+    const actual = await weatherApiService.statisticsForNumberOfDays(1)
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('should only use the temps of the last 24 hours when the last day contains only 12 hours', async () => {
+    const expected = {
+      min: 0,
+      average: 0.5,
+      max: 1,
+    }
+    const dataDay1 = {
+      hourly: new Array(24).fill({ temp: 0 }),
+    }
+    const dataDay2 = {
+      hourly: new Array(12).fill({ temp: 1 }),
+    }
+
+    const mockedFunction = jest
+      .fn()
+      .mockReturnValueOnce(Promise.resolve(dataDay1))
+      .mockReturnValueOnce(Promise.resolve(dataDay2))
+    const weatherApiService = setupWeatherApiService(mockedFunction)
+    const actual = await weatherApiService.statisticsForNumberOfDays(1)
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('should ask the weather client for the earliest date first', async () => {
+    const dataDay1 = {
+      hourly: [],
+    }
+    const dataDay2 = {
+      hourly: [],
+    }
+
+    const mockedFunction = jest
+      .fn()
+      .mockReturnValueOnce(Promise.resolve(dataDay1))
+      .mockReturnValueOnce(Promise.resolve(dataDay2))
+    const weatherApiService = setupWeatherApiService(mockedFunction)
+    await weatherApiService.statisticsForNumberOfDays(1)
+
+    const { calls } = mockedFunction.mock
+    expect(calls.length).toEqual(2)
+    expect(calls[0][0]).toBeLessThan(calls[1][0])
+  })
+})
---
+++ services/weather-service.ts
@@ -0,0 +1,68 @@
+import dayjs from 'dayjs'
+import _ from 'lodash'
+import OpenWeatherMapClient from '../clients/open-weather-map-client'
+import { WeatherApiResponse } from '../types/weather'
+
+interface PeriodStatistics {
+  max: number
+  min: number
+  average: number
+}
+
+class WeatherService {
+  constructor(private readonly openWeatherMapClient: OpenWeatherMapClient) {}
+
+  public async statisticsForNumberOfDays(numberOfDays: number): Promise<PeriodStatistics> {
+    const days = WeatherService.timestampsInterval(numberOfDays + 1)
+    const apiResponses = await Promise.all(
+      days.map((day) => {
+        // js uses milliseconds for unix time while the api uses seconds
+        const dayInSeconds = day / 1000
+        return this.openWeatherMapClient.weatherForDay(dayInSeconds)
+      })
+    )
+    const allTemps = WeatherService.apiResponseToFlatTempsPerHour(apiResponses, numberOfDays)
+    const precision = 2
+    const max = _.round(_.max(allTemps) || 0, precision)
+    const min = _.round(_.min(allTemps) || 0, precision)
+    const average = _.round(_.mean(allTemps), precision)
+    return { max, min, average }
+  }
+
+  /**
+   *
+   * @param days the number of days back that should be included in the interval
+   * @param naturalOrder should the interval be returned in a chronological order, defaults to true
+   * @param start the day in which the interval start. Should be given as unix milliseconds, defaults to today
+   */
+  private static timestampsInterval(
+    days: number,
+    naturalOrder = true,
+    start = dayjs().startOf('day')
+  ): number[] {
+    const interval = []
+    let current = start
+    for (let i = 0; i < days; i += 1) {
+      interval.push(+current)
+      current = current.subtract(1, 'day')
+    }
+    return naturalOrder ? _.reverse(interval) : interval
+  }
+
+  private static apiResponseToFlatTempsPerHour(
+    responses: WeatherApiResponse[],
+    numberOfDays: number,
+    even24Hours = true
+  ): number[] {
+    const tempsByDay = responses.map((response) => response.hourly.map((hourly) => hourly.temp))
+    const allTemps = _.flattenDeep(tempsByDay)
+    if (even24Hours) {
+      const hoursInADay = 24
+      const excessDaysInFirstDay = allTemps.length - hoursInADay * numberOfDays
+      allTemps.splice(0, excessDaysInFirstDay)
+    }
+    return allTemps
+  }
+}
+
+export default WeatherService
