13f70815a62461343f328ddd0024a011b731034d
anthager <anthage@student.chalmers.se>
2021-03-08 20:53:23 +0100

    use typing in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
